a1e49dadf48fece9652186229b6f79ce9a4d3f09
Jamie McCallum <jamiemccallum0@gmail.com>
2024-11-11 22:18:50 +0000

    fixes and proper build implementation

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
-let saveEl = document.getElementById("save-el")
-let countEl = document.getElementById("count-el")
-let count = 0
+let saveEl = document.getElementById("save-el");
+let countEl = document.getElementById("count-el");
+let incrementBtn = document.getElementById("increment-btn");
+let saveBtn = document.getElementById("save-btn");
+let count = 0;
+
+incrementBtn.addEventListener("click", increment);
+
+saveBtn.addEventListener("click", save);
 
 function increment() {
-    count += 1
-    countEl.textContent = count
+    count += 1;
+    countEl.textContent = count;
 }
 
 function save() {
-    let countStr = count + " - "
-    saveEl.textContent += countStr
-    countEl.textContent = 0
-    count = 0
+    let countStr = count + " - ";
+    saveEl.textContent += countStr;
+    countEl.textContent = 0;
+    count = 0;
 }
